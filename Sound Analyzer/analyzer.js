8e829bead3f6423105ff1b22ec5bb5d66152a7bb
mreyes2000 <55515152+mreyes2000@users.noreply.github.com>
2019-12-06 14:19:41 -0500

    Update analyzer.js

--- Sound Analyzer/analyzer.js
+++ Sound Analyzer/analyzer.js
@@ -7,7 +7,7 @@ function callFunction(){
   }
 
 
-  d3.csv("20191004_002_LLUFTest_ShiviLaura_C100a_001_audiodata.csv")
+  d3.csv("https://mreyes2000.github.io/Sound%20Analyzer/20191004_002_LLUFTest_ShiviLaura_C100a_001_audiodata.csv")
       .row(function(d){ return {time: Number(d.time), amp: Number(d.amp)};})
       .get(function(error,data){
 
